cbbde73997dd7831711d0c54a8c5314364cfa242
Canh Trinh <canh@axelar.network>
2021-09-10 10:37:57 -0400

    refactor(rest): code cleanup #6

---
+++ axelarjs-rest-server/src/Plugins/client-rest-routes/post/transferAsset.ts
@@ -0,0 +1,40 @@
+import {Request, ResponseToolkit} from "@hapi/hapi";
+import {
+	CLIENT_API_POST_TRANSFER_ASSET,
+	IAssetTransferObject,
+	IDepositAddressResponse
+} from "@axelar-network/axelarjs-sdk";
+import Boom from "@hapi/boom";
+
+interface AssetTransferRequest extends Request {
+	payload: IAssetTransferObject;
+}
+
+export const transferAsset = {
+	method: 'POST',
+	path: CLIENT_API_POST_TRANSFER_ASSET,
+	handler: function (request: AssetTransferRequest, h: ResponseToolkit) {
+
+		try {
+
+			let payload: IAssetTransferObject = request.payload as IAssetTransferObject;
+
+			console.log("post request", payload);
+
+			const responseObj: IDepositAddressResponse = {
+				destinationTokenDepositAddress: "123456789", // TODO: will come from downstream service
+				destinationTokenSymbol: payload?.destinationTokenSymbol
+			}
+
+			return h.response(responseObj);
+
+		} catch (e: any) {
+
+			console.log(e);
+			throw Boom.boomify(e, { statusCode: 400 });
+
+		}
+
+	}
+
+}
--- axelarjs-rest-server/src/Plugins/client-rest-routes/routes.ts
+++ axelarjs-rest-server/src/Plugins/client-rest-routes/routes.ts
@@ -1,16 +1,7 @@
 'use strict';
 
-import {Request, ResponseToolkit, Server} from "@hapi/hapi";
-import {
-	CLIENT_API_POST_TRANSFER_ASSET,
-	IAssetTransferObject,
-	IDepositAddressResponse
-} from "@axelar-network/axelarjs-sdk";
-import Boom from "@hapi/boom";
-
-interface AssetTransferRequest extends Request {
-	payload: IAssetTransferObject;
-}
+import {Request, Server} from "@hapi/hapi";
+import {transferAsset} from "./post/transferAsset";
 
 export const routes = {
 	name: 'routes',
@@ -21,33 +12,12 @@ export const routes = {
 			method: 'GET',
 			path: '/',
 			handler: (request: Request): string => {
-
 				console.log("Processing request", request.info.id);
 				return "This is working!";
-
 			}
 		});
 
-		server.route({
-			method: 'POST',
-			path: CLIENT_API_POST_TRANSFER_ASSET,
-			handler: function (request: AssetTransferRequest, h: ResponseToolkit) {
-
-				try {
-					let payload: IAssetTransferObject;
-					payload = request.payload as IAssetTransferObject;
-					console.log("post request", payload);
-					const responseObj: IDepositAddressResponse = {
-						destinationTokenDepositAddress: "123456789",
-						destinationTokenSymbol: payload?.destinationTokenSymbol
-					}
-					return h.response(responseObj);
-				} catch (e: any) {
-					console.log(e);
-					throw Boom.boomify(e, { statusCode: 500});
-				}
-			}
-		});
+		server.route(transferAsset);
 
 		// etc ...
 		// await someAsyncMethods(); what does this do?
