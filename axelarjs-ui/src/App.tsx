ddd86b683b9b02c96228f8c15005643cae7fdb88
Canh Trinh <canh@axelar.network>
2021-09-10 23:06:53 -0400

    fix(ui): restoring main view on app.tsx file

--- axelarjs-ui/src/App.tsx
+++ axelarjs-ui/src/App.tsx
@@ -1,53 +1,44 @@
 import React from 'react';
-// import {IAssetTransferObject, TransferAssetBridge} from "@axelar-network/axelarjs-sdk";
+import {IAssetTransferObject, TransferAssetBridge} from "@axelar-network/axelarjs-sdk";
 
 import './App.css';
-import {AddTodo} from "./component/ignored-files/AddTodo";
-import {TodoList} from "./component/ignored-files/TodoList";
 
-// const axelarBridgeFacade = new TransferAssetBridge("http://localhost:4000");
+
+const axelarBridgeFacade = new TransferAssetBridge("http://localhost:4000");
 
 const App = () => {
 
 
-	// const message: IAssetTransferObject = {
-	// 	sourceTokenSymbol: "BTC",
-	// 	destinationTokenSymbol: "ETH",
-	// 	destinationAddress: "TBD"
-	// }
+	const message: IAssetTransferObject = {
+		sourceTokenSymbol: "BTC",
+		destinationTokenSymbol: "ETH",
+		destinationAddress: "TBD"
+	}
 
-	// const onClick = async () => {
-	//
-	// 	const res = await axelarBridgeFacade.transferAssets(message, console.log);
-	// 	console.log("results",res);
-	// }
+	const onClick = async () => {
+
+		const res = await axelarBridgeFacade.transferAssets(message, console.log);
+		console.log("results",res);
+	}
 
 	return (
-		<div>
-			<AddTodo />
-			<TodoList />
+		<div className="App">
+			<header className="App-header">
+			<p>
+				Edit <code>src/App.tsx</code> and save to reload.
+			</p>
+			<div onClick={onClick}>Click me!</div>
+			<a
+				className="App-link"
+				href="https://reactjs.org"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				Learn React
+			</a>
+			</header>
 		</div>
-	);
-
-	// return (
-	// 	<div className="App">
-	// 		<header className="App-header">
-	// 		<img src={logo} className="App-logo" alt="logo" />
-	// 		<p>
-	// 			Edit <code>src/App.tsx</code> and save to reload.
-	// 		</p>
-	// 		<div onClick={onClick}>Click me!</div>
-	// 		<a
-	// 			className="App-link"
-	// 			href="https://reactjs.org"
-	// 			target="_blank"
-	// 			rel="noopener noreferrer"
-	// 		>
-	// 			Learn React
-	// 		</a>
-	// 		</header>
-	// 	</div>
-  // );
+  );
 }
 
 export default App;
