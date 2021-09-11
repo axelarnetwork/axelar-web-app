c46e0940e292199ae6f3b743553ed4a779e61466
Canh Trinh <canh@axelar.network>
2021-09-10 23:04:15 -0400

    documentation(ui): adding recoil and recoil logger #5

--- axelarjs-ui/src/App.tsx
+++ axelarjs-ui/src/App.tsx
@@ -1,45 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import {IAssetTransferObject, TransferAssetBridge} from "@axelar-network/axelarjs-sdk";
+// import {IAssetTransferObject, TransferAssetBridge} from "@axelar-network/axelarjs-sdk";
 
 import './App.css';
+import {AddTodo} from "./component/ignored-files/AddTodo";
+import {TodoList} from "./component/ignored-files/TodoList";
 
-const axelarBridgeFacade = new TransferAssetBridge("http://localhost:4000");
+// const axelarBridgeFacade = new TransferAssetBridge("http://localhost:4000");
 
 const App = () => {
 
 
-	const message: IAssetTransferObject = {
-		sourceTokenSymbol: "BTC",
-		destinationTokenSymbol: "ETH",
-		destinationAddress: "TBD"
-	}
+	// const message: IAssetTransferObject = {
+	// 	sourceTokenSymbol: "BTC",
+	// 	destinationTokenSymbol: "ETH",
+	// 	destinationAddress: "TBD"
+	// }
 
-	const onClick = async () => {
-
-		const res = await axelarBridgeFacade.transferAssets(message, console.log);
-		console.log("results",res);
-	}
+	// const onClick = async () => {
+	//
+	// 	const res = await axelarBridgeFacade.transferAssets(message, console.log);
+	// 	console.log("results",res);
+	// }
 
 	return (
-		<div className="App">
-			<header className="App-header">
-			<img src={logo} className="App-logo" alt="logo" />
-			<p>
-				Edit <code>src/App.tsx</code> and save to reload.
-			</p>
-			<div onClick={onClick}>Click me!</div>
-			<a
-				className="App-link"
-				href="https://reactjs.org"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				Learn React
-			</a>
-			</header>
+		<div>
+			<AddTodo />
+			<TodoList />
 		</div>
-  );
+	);
+
+	// return (
+	// 	<div className="App">
+	// 		<header className="App-header">
+	// 		<img src={logo} className="App-logo" alt="logo" />
+	// 		<p>
+	// 			Edit <code>src/App.tsx</code> and save to reload.
+	// 		</p>
+	// 		<div onClick={onClick}>Click me!</div>
+	// 		<a
+	// 			className="App-link"
+	// 			href="https://reactjs.org"
+	// 			target="_blank"
+	// 			rel="noopener noreferrer"
+	// 		>
+	// 			Learn React
+	// 		</a>
+	// 		</header>
+	// 	</div>
+  // );
 }
 
 export default App;
--- axelarjs-ui/src/index.tsx
+++ axelarjs-ui/src/index.tsx
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {RecoilRoot} from "recoil";
 
+import { RecoilLogger } from 'recoil-devtools-logger';
+
+// @ts-ignore
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <RecoilRoot>
+            <RecoilLogger />
+            <App />
+        </RecoilRoot>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
