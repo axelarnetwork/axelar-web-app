b996165915a3911f16b156faa1e08e7d1f6f4e1f
Canh Trinh <canhtrinh@users.noreply.github.com>
2021-10-13 10:24:52 -0400

    fix(rest): types code cleanup (#18)
    * fix(rest): types code cleanup
    
    * fix(rest): reformatting

--- axelarjs-rest-server/src/services/AxelarMicroservices/index.ts
+++ axelarjs-rest-server/src/services/AxelarMicroservices/index.ts
@@ -1,5 +1,5 @@
-import {AxiosInstance, AxiosRequestConfig}                               from "axios";
-import {INonSmartContractLinkRequestBody, ISmartContractLinkRequestBody} from "@axelar-network/axelarjs-sdk";
+import {AxiosInstance, AxiosRequestConfig}        from "axios";
+import {IBTCLinkRequestBody, IEVMLinkRequestBody} from "@axelar-network/axelarjs-sdk";
 
 export class AxelarMicroservices {
 
@@ -10,15 +10,16 @@ export class AxelarMicroservices {
 	constructor() {
 		this.MICROSERVICES_HOSTNAME = process.env.MICROSERVICES_URL as string;
 		this.axios = require("axios")
-			.create({
-				baseUrl: this.MICROSERVICES_HOSTNAME,
-				headers: {'Access-Control-Allow-Origin' : '*',
-					'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS'
-				}
-			} as AxiosRequestConfig);
+		.create({
+			baseUrl: this.MICROSERVICES_HOSTNAME,
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+			}
+		} as AxiosRequestConfig);
 	}
 
-	public async link(body: INonSmartContractLinkRequestBody | ISmartContractLinkRequestBody) {
+	public async link(body: IBTCLinkRequestBody | IEVMLinkRequestBody) {
 		try {
 			const res = await this.axios.post(this.MICROSERVICES_HOSTNAME + this.LINK_URL, body)
 			return res.data;
