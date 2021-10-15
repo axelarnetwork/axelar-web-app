fe3d1aa45a83a955015cdcd748ea2cd767570a53
Canh Trinh <canh@axelar.network>
2021-10-15 14:52:56 -0400

    fix(ui): code cleanup

--- axelarjs-ui/src/utils/utils.ts
+++
@@ -1,6 +0,0 @@
-import {validateDestinationAddress} from "@axelar-network/axelarjs-sdk/dist/utils";
-import {IAsset}                     from "@axelar-network/axelarjs-sdk";
-
-export const validateAddr = (addrInfo: IAsset, cb?: any) => {
-	return validateDestinationAddress(addrInfo);
-}
