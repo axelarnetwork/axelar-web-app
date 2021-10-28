d12ee3774490981d030aa12fdd7d1f4481abf57e
Canh Trinh <canh@axelar.network>
2021-10-27 21:16:31 -0400

    chore(ui): fixing environment variable issue on the ui side

--- axelarjs-ui/src/config/downstreamServices.ts
+++ axelarjs-ui/src/config/downstreamServices.ts
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const GITHUB_LINK: string = "https://github.com/axelarnetwork/axelar-web-app";
 const RECAPTCHA_SITE_KEY: string = "6LcxwsocAAAAANQ1t72JEcligfeSr7SSq_pDC9vR"; //this is intentionally public
 
@@ -8,7 +10,8 @@ interface IDownstreamServicesConfigs {
 	AXELAR_BRIDGE_URL: string;
 }
 
-const environment: string = process.env.REST_SERVER_URL as string;
+const REST_SERVER_URL: string = process.env.REACT_APP_REST_SERVER_URL as string;
+
 const setConfigs = (AXELAR_BRIDGE_URL: string): IDownstreamServicesConfigs => ({
 	AXELAR_BRIDGE_URL
 })
@@ -16,9 +19,9 @@ const setConfigs = (AXELAR_BRIDGE_URL: string): IDownstreamServicesConfigs => ({
 const configsByEnvironment: { [key: string]: IDownstreamServicesConfigs } = {};
 
 configsByEnvironment.local = setConfigs("http://localhost:4000");
-configsByEnvironment.devnet = setConfigs(environment);
-configsByEnvironment.testnet = setConfigs(environment);
-configsByEnvironment.mainnet = setConfigs(environment);
+configsByEnvironment.devnet = setConfigs(REST_SERVER_URL);
+configsByEnvironment.testnet = setConfigs(REST_SERVER_URL);
+configsByEnvironment.mainnet = setConfigs(REST_SERVER_URL);
 
 const getEnvironmentBasedConfigs = (environment: string) => {
 	return configsByEnvironment[environment || "devnet"];
