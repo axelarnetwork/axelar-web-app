2f16804efddf7755f2dbe59f84a59ec6fae6d234
Canh Trinh <canhtrinh@users.noreply.github.com>
2021-10-28 14:42:20 -0400

    automated UI deployment to Vercel (#40)
    * Create lower-ui-vercel.yml
    
    * Update lower-ui-vercel.yml
    
    * Update lower-ui-vercel.yml
    
    * Update lower-ui-vercel.yml
    
    fixing relative path
    
    * Update lower-ui-vercel.yml
    
    fixing another place where relative path appears
    
    * Update lower-ui-vercel.yml
    
    replacing secrets file
    
    * Update lower-ui-vercel.yml
    
    updating "uses" lines
    
    * Update lower-ui-vercel.yml
    
    trying a simpler one
    
    * Update lower-ui-vercel.yml
    
    updating aliases
    
    * Update lower-ui-vercel.yml
    
    adding axelar team ID in vercel as scope param
    
    * Update lower-ui-vercel.yml
    
    removing custom CNAME that isn't registered yet
    
    * Update and rename lower-ui-vercel.yml to deploy-ui-to-devnet-and-testnet.yml
    
    renaming job
    
    * Update deploy-ui-to-devnet-and-testnet.yml
    
    adding testnet configs
    
    * Update deploy-ui-to-devnet-and-testnet.yml
    
    removing unused comment
    
    * Update deploy-ui-to-devnet-and-testnet.yml
    
    * Update deploy-ui-to-devnet-and-testnet.yml
    
    reverting for testing purposes
    
    * fix(ui): fixing typo
    
    * Update deploy-ui-to-devnet-and-testnet.yml
    
    changing branch back to develop
    
    * chore(ui): refactoring the environment configs object in ui/downstreamServices

--- axelarjs-ui/src/config/downstreamServices.ts
+++ axelarjs-ui/src/config/downstreamServices.ts
@@ -3,34 +3,16 @@ require('dotenv').config()
 const GITHUB_LINK: string = "https://github.com/axelarnetwork/axelar-web-app";
 const RECAPTCHA_SITE_KEY: string = "6LcxwsocAAAAANQ1t72JEcligfeSr7SSq_pDC9vR"; //this is intentionally public
 
-// eslint-disable-next-line
-const AXELAR_BRIDGE_URL: string = "AXELAR_BRIDGE_URL";
-
-interface IDownstreamServicesConfigs {
+interface IConfig {
+	GITHUB_LINK: string;
+	RECAPTCHA_SITE_KEY: string;
 	AXELAR_BRIDGE_URL: string;
 }
 
-const REST_SERVER_URL: string = process.env.REACT_APP_REST_SERVER_URL as string;
-
-const setConfigs = (AXELAR_BRIDGE_URL: string): IDownstreamServicesConfigs => ({
-	AXELAR_BRIDGE_URL
-})
-
-const configsByEnvironment: { [key: string]: IDownstreamServicesConfigs } = {};
-
-configsByEnvironment.local = setConfigs("http://localhost:4000");
-configsByEnvironment.devnet = setConfigs(environment);
-configsByEnvironment.testnet = setConfigs(environment);
-configsByEnvironment.mainnet = setConfigs(environment);
-
-const getEnvironmentBasedConfigs = (environment: string) => {
-	return configsByEnvironment[environment || "devnet"];
-}
-
-const configs = {
+const configs: IConfig = {
 	GITHUB_LINK,
 	RECAPTCHA_SITE_KEY,
-	getEnvironmentBasedConfigs
+	AXELAR_BRIDGE_URL: process.env.REACT_APP_REST_SERVER_URL as string
 }
 
 export default configs;
