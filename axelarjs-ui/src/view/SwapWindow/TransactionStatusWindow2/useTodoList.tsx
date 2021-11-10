7ead997c9e08f4466a9766ab6db74fc80ae93da0
Canh Trinh <canh@axelar.network>
2021-11-09 20:50:52 -0500

    chore: cleaning up styles in transaction status page

--- axelarjs-ui/src/view/SwapWindow/TransactionStatusWindow2/useTodoList.tsx
+++ axelarjs-ui/src/view/SwapWindow/TransactionStatusWindow2/useTodoList.tsx
@@ -1,21 +1,23 @@
 import React, {ReactElement, useState}   from 'react';
-import {Container, ListGroup,}           from 'react-bootstrap';
 import {CSSTransition, TransitionGroup,} from 'react-transition-group';
-// import styled                            from "styled-components";
+import styled                            from "styled-components";
 import {v4 as uuid}                      from 'uuid';
 import {useSetRecoilState}               from "recoil";
 import {FlexSpaceBetween}                from "component/StyleComponents/FlexSpaceBetween";
-import {SVGImage}                        from "component/Widgets/SVGImage";
+import {StyledImage}                     from "component/StyleComponents/StyledImage";
 import {ShowHelperCartoonWidget}         from "state/ApplicationStatus";
 import './todolist.css';
 
-// const StyledListItem = styled.div`
-//     position: relative;
-//     display: block;
-//     padding: 0.75rem 1.25rem;
-//     background-color: #fff;
-//     height: 30%;
-// `;
+const StyledListItem = styled.div`
+    position: relative;
+    display: block;
+    padding: 0.75rem 1.25rem;
+    background-color: #fff;
+    height: 30%;
+`;
+const StyledTransitionGroup = styled(TransitionGroup)`
+	height: 275px;
+`;
 
 const steps: (ReactElement | null)[] = [null, null, null, null];
 
@@ -33,30 +35,27 @@ const useTodoList = () => {
 	const [activeStep, setActiveStep] = useState<number>(0);
 	const setShowHelperCartoonWidget = useSetRecoilState(ShowHelperCartoonWidget);
 
-	const jsx = () => <Container style={{height: '275px'}}>
-		<ListGroup style={{marginBottom: '1rem'}}>
-			<TransitionGroup className="todo-list">
-				{items
-				.filter((item, i) => i <= activeStep)
-				.map(({id, text}: IITem, currIdx: number) => {
-					return <CSSTransition
-						key={id}
-						timeout={500}
-						classNames="item"
-					>
-						<ListGroup.Item>
-							<FlexSpaceBetween>
-							<SVGImage height={"40px"} width={"40px"}
-							          src={require(`resources/transaction_status_logos/transfer-step-${currIdx + 1}-${currIdx === activeStep ? "active" : "inactive"}.svg`).default}
-							/>
-							<div style={{ width: `100%`, textIndent: `20px`, height: `100%` }}>{text}</div>
-							</FlexSpaceBetween>
-						</ListGroup.Item>
-					</CSSTransition>
-				})}
-			</TransitionGroup>
-		</ListGroup>
-	</Container>;
+	const jsx = () => <StyledTransitionGroup>
+		{items
+		.filter((item, i) => i <= activeStep)
+		.map(({id, text}: IITem, currIdx: number) => {
+			const img = require(`resources/transaction_status_logos/transfer-step-${currIdx + 1}-${currIdx === activeStep ? "active" : "inactive"}.svg`).default;
+			return <CSSTransition
+				key={id}
+				timeout={500}
+				classNames="item"
+			>
+				<StyledListItem>
+					<FlexSpaceBetween>
+						<div style={{ width: `40px`, height: `40px`, position: `relative` }}>
+							<StyledImage src={img} />
+						</div>
+					<div style={{ width: `100%`, textIndent: `20px`, height: `100%` }}>{text}</div>
+					</FlexSpaceBetween>
+				</StyledListItem>
+			</CSSTransition>
+		})}
+	</StyledTransitionGroup>;
 
 	const updateStep = (itemText: ReactElement, index: number) => {
 		const newState = items.map((item: IITem, iter: number) => {
