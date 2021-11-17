d2ab5075c1b3695f6d31aeb06d4561da2cecff65
Canh Trinh <canh@axelar.network>
2021-11-17 14:39:29 -0500

    chore: improving background transition on activation

--- src/view/SwapWindow/index.tsx
+++ src/view/SwapWindow/index.tsx
@@ -1,29 +1,33 @@
 import {ReactElement}                                    from "react";
 import {CSSTransition, SwitchTransition}                 from "react-transition-group";
-import {useRecoilValue}                  from "recoil";
-import styled                                            from "styled-components";
+import {useRecoilValue}                                  from "recoil";
+import styled, {ThemedStyledProps}                       from "styled-components";
 import screenConfigs                                     from "config/screenConfigs";
 import {DESTINATION_TOKEN_KEY, SOURCE_TOKEN_KEY}         from "config/consts";
 import {animateStyles}                                   from "component/StyleComponents/animations/SwitchToggleAnimation";
-import {switchBackgroundAnimation}                                   from "component/StyleComponents/animations/SwitchBackgroundAnimation";
 import {StyledCentered}                                  from "component/StyleComponents/Centered";
 import usePostTransactionToBridge                        from "hooks/usePostTransactionToBridge";
 import {ChainSelection, DestinationAddress, SourceAsset} from "state/ChainSelection";
-import inactiveBox                                               from "resources/inactive-box.svg";
-import activeBox                                               from "resources/active-box.svg";
+import inactiveBox                                       from "resources/inactive-box.svg";
+import activeBox                                         from "resources/active-box.svg";
 import UserInputWindow                                   from "./UserInputWindow";
 import TransactionStatusWindow                           from "./TransactionStatusWindow";
 
-const StyledImage = styled.img`
+interface IStyledImageProps extends ThemedStyledProps<any, any> {
+	showContents?: boolean;
+}
+
+const StyledImage = styled.img<IStyledImageProps>`
 	position: absolute;
 	width: 1256px;
 	height: 533px;
+	opacity: ${props => props.showContents ? "1" : "0"};
+	${props => props.showContents ? `transition: opacity 500ms ease-in;` : `transition: opacity 500ms ease-out; transition-delay: 500ms;`}
 `;
 
 const StyledSwapWindow = styled.div`
 	${StyledCentered}
 	${animateStyles}
-	${switchBackgroundAnimation}
     position: relative;
     width: 100%;
     height: 100%;
@@ -70,21 +74,15 @@ const SwapWindow = (): ReactElement => {
 
 	return <StyledSwapWindow>
 
-		<SwitchTransition mode={"out-in"}>
-			<CSSTransition
-				key={canLightUp ? "lit-background" : "dimmed-background"}
-				addEndListener={(node, done) => node.addEventListener("transitionend", done, false)}
-				classNames="lighten"
-			>{
-				canLightUp ? <StyledImage src={activeBox} /> : <StyledImage src={inactiveBox} />
-			}</CSSTransition>
-		</SwitchTransition>
+		<StyledImage src={activeBox} showContents={canLightUp}/>
+		<StyledImage src={inactiveBox} showContents={!canLightUp}/>
+
 		<SwitchTransition mode={"out-in"}>
 			<CSSTransition
 				key={userInputNeeded ? "user-input-window" : "transaction-status-window"}
 				addEndListener={(node, done) => node.addEventListener("transitionend", done, false)}
 				classNames="fade"
-			><StyledContainer>{ userInputNeeded
+			><StyledContainer>{userInputNeeded
 				? <UserInputWindow handleSwapSubmit={handleTransactionSubmission}/>
 				: <TransactionStatusWindow isOpen={showTransactionStatusWindow}
 				                           closeResultsScreen={closeResultsScreen}/>
