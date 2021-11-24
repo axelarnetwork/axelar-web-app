584afc3f77ddd63f6346f0da391398f0c9476501
Canh Trinh <canh@axelar.network>
2021-11-23 22:37:01 -0500

    chore: minor word cleanup to transfer fee divider

--- src/component/CompositeComponents/TransferFeeDivider.tsx
+++ src/component/CompositeComponents/TransferFeeDivider.tsx
@@ -3,14 +3,15 @@ import BoldSpan                   from "component/StyleComponents/BoldSpan";
 import {StyledImage}              from "component/StyleComponents/StyledImage";
 import {StyledTransferFeeDivider} from "component/StyleComponents/StyledTransferFeeDivider";
 import {SOURCE_TOKEN_KEY}         from "config/consts";
-import dividerImage               from "resources/group.svg";
-import {ChainSelection}           from "state/ChainSelection";
+import dividerImage                  from "resources/group.svg";
+import {ChainSelection, SourceAsset} from "state/ChainSelection";
 
 const TransferFeeDivider = () => {
 
 	const sourceChainSelection = useRecoilValue(ChainSelection(SOURCE_TOKEN_KEY));
+	const selectedSourceAsset = useRecoilValue(SourceAsset);
 
-	return <StyledTransferFeeDivider showContents={!!sourceChainSelection}>
+	return <StyledTransferFeeDivider showContents={!!(sourceChainSelection && selectedSourceAsset)}>
 		<StyledImage src={dividerImage}/>
 		<div style={{display: `flex`, justifyContent: `space-between`}}>
 			<div style={{marginLeft: `15px`, marginTop: `10px`}}>
@@ -19,7 +20,7 @@ const TransferFeeDivider = () => {
 			</div>
 			<div style={{textAlign: `right`}}>
 				<div><BoldSpan>Transfer Fee: </BoldSpan></div>
-				<div>XX% of transferred tokens on {sourceChainSelection?.chainName}</div>
+				<div>XX% of transferred {selectedSourceAsset?.assetSymbol}</div>
 			</div>
 		</div>
 	</StyledTransferFeeDivider>;
