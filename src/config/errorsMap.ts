e2b759a8a74da9944a9efdc1e0af20264bc74b2e
Canh Trinh <canh@axelar.network>
2021-11-24 16:36:04 -0500

    chore: adding rate limit error messaging

--- src/config/errorsMap.ts
+++ src/config/errorsMap.ts
@@ -4,6 +4,7 @@ export const getErrorMessage: (errorCode: number, error: any) => string = (error
 		503: `Unexpected error occurred, try again later.`,
 		403: error.message + "; Refresh this page and try again.", //handle recaptcha error
 		400: error.message + "; Check your inputs and try again.", //handle bad request error
+		429: error.message + "; Try again in a few minutes.", //handle rate limit error
 	};
 
 	if (errorsMap[errorCode])
