cc262904ddb09bb17da495ce9e6297cc80c916b6
Canh Trinh <canh@axelar.network>
2021-12-02 14:31:50 -0500

    chore: code cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -66,13 +66,6 @@ const GlobalStyle = createGlobalStyle`
 	
 		}		
 	}
-	
-	select {
-	  -webkit-appearance: none;
-	  -moz-appearance: none;
-	  appearance: none;
-	  background: url(https://stackoverflow.com/favicon.ico) 96% / 15% no-repeat #EEE;
-	}
 
 	code {
 	    // font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
