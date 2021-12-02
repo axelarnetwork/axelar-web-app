209699bbb0aca872cd60b313cfa0fe6a4078411d
Canh Trinh <canh@axelar.network>
2021-12-02 11:21:39 -0500

    feat: updated support center

--- src/view/FAQPage/index.tsx
+++ src/view/FAQPage/index.tsx
@@ -4,47 +4,125 @@ import styled                                     from "styled-components";
 import Tooltip                                    from "component/Widgets/Tooltip";
 import CopyToClipboard                            from "component/Widgets/CopyToClipboard";
 import BoldSpan                                   from "component/StyleComponents/BoldSpan";
+import {FlexRow}                                  from "component/StyleComponents/FlexRow";
+import {useState}                                 from "react";
 
-const StyledFAQPage = styled.div`
+const StyledHelperComponent = styled.div`
+    position: absolute;
+    z-index: 15;
+    bottom: 50px;
+    right: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    width: 30%;
+    min-width: 275px;
+    max-width: 300px;
+`;
+
+const HelperWidget = styled.div`
+	box-sizing: border-box;
+	padding: 0.5em 0.75em 0.5em 0.75em;
+	margin: 0.5em;
+	background-color: black;
+	border-radius: 50px;
+	color: white;
+	cursor: pointer;
+`;
+
+const StyledFAQPopup = styled.div`
 	background-color: rgb(255,255,255,0.9);
 	color: black;
-	width: 50vw;
-	height: 60vh;
 	box-sizing: border-box;
-	border: 8px solid darkgrey;
-	padding: 1em;
-	border-radius: 5px;
+	border: 2px solid #b9bac8;
+	border-radius: 10px;
 	overflow-wrap: break-word;
+    background-color: white;
+    width: 100%;
+    font-size: 12px;
+`;
 
+const StyledHeader = styled.div`
+	position: relative;
+	width: 100%;
+    background-color: black;
+    color: white;
+    text-align: center;
+    font-size: large;
+    box-sizing: border-box;
+    padding: 0.25em;
+`;
+
+const FAQSection = styled.div`
+    box-sizing: border-box;
+    padding: 0.75em;
+`;
+
+const ContactUsSection = styled(FAQSection)`
+	background-color: #bab9c8;
+	padding-bottom: 0.1em;
 `;
 
 const FAQPage = () => {
 	const transactionTraceId = useRecoilValue(TransactionTraceId);
 	const depositAddress = useRecoilValue(SourceDepositAddress);
+	const [showFAQ, setShowFAQ] = useState(false);
 
-	return <StyledFAQPage>
-		<h1>Frequently Asked Questions</h1>
-		<div>TBU</div>
-		<h1>Having issues with a live transaction?</h1>
-		<div>Reach out to us on Discord with the following info</div>
-		<br/>
-
-		<Tooltip
-			anchorContent={<CopyToClipboard
-				JSXToShow={<>
-					<div>Trace ID: <BoldSpan>{transactionTraceId}</BoldSpan></div>
-					<div>{depositAddress ? <span>Deposit Address -
-						<BoldSpan>{depositAddress?.assetAddress}</BoldSpan></span> : null}</div>
-				</>}
-				height={`12px`}
-				width={`10px`}
-				textToCopy={JSON.stringify({transactionTraceId: transactionTraceId, ...(depositAddress)})}
-				showImage={false}
-			/>}
-			tooltipText={(transactionTraceId && depositAddress ? "Copy both to Clipboard" : "Copy to Clipboard")}
-			tooltipAltText={"Copied to Clipboard!"}
-		/>
-	</StyledFAQPage>;
+	return <StyledHelperComponent>
+		{ showFAQ && <StyledFAQPopup>
+            <StyledHeader>
+                <span>Frequently Asked Questions</span>
+                <div
+	                style={{ position: `absolute`, right: 8, top: 5, cursor: `pointer` }}
+	                onClick={() => setShowFAQ(false)}
+                >
+	                <img src={require(`resources/close-icon.svg`).default} alt={""} />
+                </div>
+            </StyledHeader>
+            <FAQSection>
+                <div><BoldSpan>Question 1?</BoldSpan></div>
+                <div>TBU</div>
+                <br/>
+                <div><BoldSpan>Question 2?</BoldSpan></div>
+                <div>TBU</div>
+            </FAQSection>
+            <ContactUsSection>
+                <br/>
+                <div><BoldSpan>Having issues with a live transaction?</BoldSpan></div>
+                <br/>
+                <div style={{ marginBottom: `5px` }}>Reach out to us on Discord, referencing</div>
+                <Tooltip
+                    anchorContent={<CopyToClipboard
+						JSXToShow={<>
+							<div>
+								<div><BoldSpan>Trace ID:</BoldSpan></div>
+								{transactionTraceId}
+							</div>
+							<div>{depositAddress
+								? <div style={{marginTop: `10px`}}>
+									<div><BoldSpan>Deposit Address:</BoldSpan></div>
+									{depositAddress?.assetAddress}
+								</div>
+								: null}
+							</div>
+						</>}
+						height={`12px`}
+						width={`10px`}
+						textToCopy={JSON.stringify({transactionTraceId: transactionTraceId, ...(depositAddress)})}
+						showImage={false}
+					/>}
+                    tooltipText={(transactionTraceId && depositAddress ? "Copy both to Clipboard" : "Copy to Clipboard")}
+                    tooltipAltText={"Copied to Clipboard!"}
+                />
+            </ContactUsSection>
+        </StyledFAQPopup>}
+		<HelperWidget onClick={() => setShowFAQ(!showFAQ)}>
+			<FlexRow>
+				<img src={require(`resources/active-eye.svg`).default} alt={""} />
+				<div style={{ marginLeft: `10px`}}>Support Center</div>
+			</FlexRow>
+		</HelperWidget>
+	</StyledHelperComponent>;
 }
 
 export default FAQPage;
