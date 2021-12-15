af349a7cc2495a1f0cf9de914dfeee40c41f9900
Canh Trinh <canhtrinh@users.noreply.github.com>
2021-12-14 20:46:57 -0500

    chore adding link to block explorer (#78)

--- src/config/downstreamServices.ts
+++ src/config/downstreamServices.ts
@@ -7,12 +7,29 @@ interface IConfig {
 	GITHUB_LINK: string;
 	RECAPTCHA_SITE_KEY: string;
 	AXELAR_BRIDGE_URL: string;
+	blockExplorers: { [environment: string]: { [chain: string]: { name: string; url: string; } } }
 }
+
+const blockExplorers = {
+	devnet: {
+		ethereum: { name: "Etherscan", url: "https://ropsten.etherscan.io/tx/" },
+		moonbeam: { name: "Moonscan", url: "https://moonbase.moonscan.io/tx/" },
+		avalanche: { name: "Snowtrace", url: "https://testnet.snowtrace.io/tx/" },
+		polygon: { name: "Polygonscan", url: "https://mumbai.polygonscan.com/tx/" },
+		fantom: { name: "FTMScan", url: "https://testnet.ftmscan.com/tx/" }
+	},
+	testnet: {},
+	local: {},
+	mainnet: {}
+}
+blockExplorers.testnet = blockExplorers.devnet;
+blockExplorers.local = blockExplorers.devnet;
 
 const configs: IConfig = {
 	GITHUB_LINK,
 	RECAPTCHA_SITE_KEY,
-	AXELAR_BRIDGE_URL: process.env.REACT_APP_REST_SERVER_URL as string
+	AXELAR_BRIDGE_URL: process.env.REACT_APP_REST_SERVER_URL as string,
+	blockExplorers
 }
 
 export default configs;
