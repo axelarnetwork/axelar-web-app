fb6510039a0857aa88512e800a8911a00c0bce91
vladwulf <agaev.vladimir@gmail.com>
2022-02-09 13:50:05 +0000

    abstract global styles away from index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -17,66 +17,7 @@ import Landing from "view/Landing"
 import App from "view/App"
 import "react-notifications-component/dist/theme.css"
 import "./index.css"
-
-const GlobalStyle = createGlobalStyle`
-
-	body {
-		font-family: ‘Gilroy-ExtraBold’, sans-serif;
-		margin: 0 auto;
-		padding: 0;
-		${HideGrecaptchaBadge}
-		${PageOpacityAnimation}
-		box-sizing: border-box;
-		position: relative;
-		line-height: initial;
-		background-image: url(${backgroundImage});
-		background-repeat: no-repeat;
-		background-position: 50% 50%;
-		background-attachment: fixed;
-		background-size: cover;
-	    max-width: 100%;
-        overflow-x: hidden;
-
-		@media ${screenConfigs.media.desktop} {
-			font-size: 20px;
-		}
-		@media ${screenConfigs.media.laptop} {
-			font-size: 18px;
-		}
-		@media ${screenConfigs.media.tablet} {
-			font-size: 16px;
-		}
-		@media ${screenConfigs.media.mobile} {
-			
-		}
-	}
-	
-	p {
-		margin-block-start: 5px;
-	    margin-block-end: 5px;
-	    margin-inline-start: 0;
-	    margin-inline-end: 0;
-	}
-	
-	button {
-		@media ${screenConfigs.media.desktop} {
-			font-size: 20px;
-		}
-		@media ${screenConfigs.media.laptop} {
-			font-size: 18px;
-		}
-		@media ${screenConfigs.media.tablet} {
-			font-size: 16px;
-		}
-		@media ${screenConfigs.media.mobile} {
-	
-		}		
-	}
-
-	code {
-	    // font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
-	}
-`
+import { GlobalStyle } from "./global-styles"
 
 new TransferAssetBridgeFacade(process.env.REACT_APP_STAGE as string)
 
