3a7ac938fe7c03873bf47ca12a8440ffd7d68f84
npty <nptytn2@gmail.com>
2022-02-16 20:25:59 +0700

    Set delay back to 5mins

--- src/config/consts.ts
+++ src/config/consts.ts
@@ -1,3 +1,3 @@
 export const SOURCE_TOKEN_KEY: string = "first-chain-selection"
 export const DESTINATION_TOKEN_KEY: string = "second-chain-selection"
-export const MS_DELAY_SHOW_CONFIRM_BTN: number = 1 * 1000 // 5 mins
+export const MS_DELAY_SHOW_CONFIRM_BTN: number = 5 * 60 * 1000 // 5 mins
