a16a435611081509b5ba0162575060c8a7097486
Canh Trinh <canh@axelar.network>
2022-03-11 11:19:44 -0500

    chore: removing unused import

--- src/hooks/useSearchParams.ts
+++ src/hooks/useSearchParams.ts
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo } from "react"
+import { useCallback, useMemo } from "react"
 import { useHistory, useLocation } from "react-router-dom"
 
 const useSearchParams = () => {
