36b5bfe8f7a9a2b516f6aac43b64e1bfe1556036
npty <78221556+npty@users.noreply.github.com>
2022-03-11 20:51:16 +0700

    Fix max button set too high value (#183)

--- src/pages/app/parts/swap-widget/TransactionStatusWindow/StatusList/DepositFromWallet.tsx
+++ src/pages/app/parts/swap-widget/TransactionStatusWindow/StatusList/DepositFromWallet.tsx
@@ -262,7 +262,8 @@ export const DepositFromWallet = ({
         )
       )
       const maxWithFee = walletBalance - fee
-      setAmountToDeposit(maxWithFee.toString())
+      const roundedMax = (Math.floor(maxWithFee * 100) / 100).toFixed(2)
+      setAmountToDeposit(roundedMax)
     } else if (
       sourceChainSelection?.chainName?.toLowerCase() === "axelar" &&
       selectedSourceAsset?.common_key === "uaxl"
@@ -274,9 +275,11 @@ export const DepositFromWallet = ({
         )
       )
       const maxWithFee = walletBalance - fee
-      setAmountToDeposit(maxWithFee.toString())
+      const roundedMax = (Math.floor(maxWithFee * 100) / 100).toFixed(2)
+      setAmountToDeposit(roundedMax)
     } else {
-      setAmountToDeposit(walletBalance.toString())
+      const roundedMax = (Math.floor(walletBalance * 100) / 100).toFixed(2)
+      setAmountToDeposit(roundedMax)
     }
   }
   const getMaxButtonText = () => {
