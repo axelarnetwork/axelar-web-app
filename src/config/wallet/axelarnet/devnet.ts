aa554a4b254ec6cba75bec7b4d85127d0ab038b3
Canh Trinh <canh@axelar.network>
2022-03-25 10:55:29 -0400

    chore: updating devnet configs

--- src/config/wallet/axelarnet/devnet.ts
+++ src/config/wallet/axelarnet/devnet.ts
@@ -1,4 +1,44 @@
-import { axelar, cosmoshub, osmosis, terra } from "./testnet"
+import { KeplrWalletChainConfig } from "./interface"
+import { ChainInfo } from "@keplr-wallet/types"
+import { Bech32Address } from "@keplr-wallet/cosmos"
+import { cosmoshub, osmosis, terra } from "./testnet"
+
+const AXELAR_CHAIN_ID: string = "axelar-devnet-rammstein-3"
+const AXELAR_RPC: string = "https://hackathon-nodes.devnet.axelar.dev"
+const AXELAR_REST = "https://hackathon-nodes-rest.devnet.axelar.dev"
+const AXELAR_CHANNEL_MAP = {
+  terra: "channel-0",
+}
+const axelarChainInfo: ChainInfo = {
+  rpc: AXELAR_RPC,
+  rest: AXELAR_REST,
+  chainId: AXELAR_CHAIN_ID,
+  chainName: "Axelar Hacknett",
+  stakeCurrency: {
+    coinDenom: "AXL",
+    coinMinimalDenom: "uaxl",
+    coinDecimals: 6,
+  },
+  bech32Config: Bech32Address.defaultBech32Config("axelar"),
+  bip44: {
+    coinType: 118,
+  },
+  currencies: [
+    { coinDenom: "AXL", coinMinimalDenom: "uaxl", coinDecimals: 6 },
+  ],
+  feeCurrencies: [
+    { coinDenom: "AXL", coinMinimalDenom: "uaxl", coinDecimals: 6 },
+  ],
+  gasPriceStep: { low: 0.05, average: 0.125, high: 0.2 },
+  features: ["stargate", "no-legacy-stdTx", "ibc-transfer"],
+}
+export const axelar: KeplrWalletChainConfig = {
+  restEndpoint: AXELAR_REST,
+  chainId: AXELAR_CHAIN_ID,
+  rpcEndpoint: AXELAR_RPC,
+  chainInfo: axelarChainInfo,
+  channelMap: AXELAR_CHANNEL_MAP,
+}
 
 const exported = {
   axelar,
