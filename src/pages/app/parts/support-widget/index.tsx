1978422368c983afb83f973125d19d88b83ed710
Canh Trinh <canh@axelar.network>
2022-04-27 21:26:51 -0400

    chore: updating youtube video

--- src/pages/app/parts/support-widget/index.tsx
+++ src/pages/app/parts/support-widget/index.tsx
@@ -156,7 +156,7 @@ export const SupportWidget = () => {
               text={"Instructional Video"}
               onClick={() =>
                 window.open(
-                  "https://www.youtube.com/watch?v=VsfCJl1A9QI",
+                  "https://www.youtube.com/watch?v=KYmWEbW69LQ",
                   "_blank"
                 )
               }
