32659332d38201c6f742520f614bdd4ea3b6953d
Canh Trinh <canh@axelar.network>
2022-05-12 12:25:41 -0400

    chore: removing terra

--- src/state/ChainList.ts
+++ src/state/ChainList.ts
@@ -11,6 +11,7 @@ const initialChainList: ChainInfo[] = loadChains({ environment })
 .filter((chain: Chain) =>
   (environment === "mainnet" ? chain.chainInfo.fullySupported : true) && !!chain?.chainInfo?.assets?.length
 )
+.filter((chain: Chain) => chain.chainInfo.chainName?.toLowerCase() !== "terra")
 .map((chain: Chain) => {
   // this is temporary given polygon RPC issues
   const newChainInfo = chain.chainInfo;
