41f46fb56f373d37b7f528817b060c316ffa205c
Canh Trinh <canh@axelar.network>
2022-05-19 14:09:55 -0400

    chore: changing RPC endpoint for juno again

--- src/config/wallet/axelarnet/mainnet.ts
+++ src/config/wallet/axelarnet/mainnet.ts
@@ -145,7 +145,7 @@ export const cosmoshub: KeplrWalletChainConfig = {
 
 export const juno: KeplrWalletChainConfig = {
   restEndpoint: "https://lcd-juno.itastakers.com",
-  rpcEndpoint: "https://rpc-juno.ecostake.com",
+  rpcEndpoint: "https://rpc.junomint.com",
   chainId: "juno-1",
   chainInfo: {
     feeCurrencies: [
