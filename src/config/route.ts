5062ce22e0a984909853955ac48dc5db3dcbc1e9
Canh Trinh <canh@axelar.network>
2022-05-26 09:28:06 -0400

    chore: updating get params to add destAddr and add asset denom

--- src/config/route.ts
+++ src/config/route.ts
@@ -1,3 +1,5 @@
 export const ROUTE_PARAM_SRC_CHAIN = "source"
 export const ROUTE_PARAM_DST_CHAIN = "destination"
 export const ROUTE_PARAM_TOKEN = "token"
+export const ROUTE_PARAM_ASSET = "asset_denom"
+export const ROUTE_PARAM_DESTINATION_ADDRESS = "destination_address"
