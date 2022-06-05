a3e245a05b76c6ba28ece3767300642276208bd8
Canh Trinh <canhtrinh@users.noreply.github.com>
2022-06-05 09:13:32 -0400

    Update src/config/feeBySourceChain.ts
    Co-authored-by: Milap Sheth <milap@axelar.network>

--- src/config/feeBySourceChain.ts
+++ src/config/feeBySourceChain.ts
@@ -3,7 +3,7 @@ import { ChainInfo } from "@axelar-network/axelarjs-sdk"
 export const feeBySourceChain = (sourceChain: ChainInfo) => {
   if (!sourceChain) return ""
 
-  if (sourceChain?.module === "axelarnet") return "~2-5 minutes"
+  if (sourceChain?.module === "axelarnet") return "~2 minutes"
 
   if (["ethereum", "polygon"].includes(sourceChain?.chainName.toLowerCase()))
     return "~15-20 minutes"
