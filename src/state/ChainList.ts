9cf8ab0fcaf58d7fe944a81970e204a0f0731370
Canh Trinh <canh@axelar.network>
2022-07-05 22:48:15 -0400

    chore: updating chain whitelisting strategy

--- src/state/ChainList.ts
+++ src/state/ChainList.ts
@@ -1,33 +1,40 @@
 import { atom } from "recoil"
+import { Chain, ChainInfo, loadChains } from "@axelar-network/axelarjs-sdk"
 import {
-  Chain,
-  ChainInfo,
-  loadChains,
-} from "@axelar-network/axelarjs-sdk"
-import { ConfigsForEnvironment, EthersJsConfigs, getConfigs } from "api/WaitService/constants";
+  ConfigsForEnvironment,
+  EthersJsConfigs,
+  getConfigs,
+} from "api/WaitService/constants"
 
 const environment = process.env.REACT_APP_STAGE as string
+const disabledChains = (process.env.REACT_APP_DISABLED_CHAINS as string) || ""
+
 const initialChainList: ChainInfo[] = loadChains({ environment })
-.filter((chain: Chain) =>
-  (environment === "mainnet" ? chain.chainInfo.fullySupported : true) && !!chain?.chainInfo?.assets?.length
-)
-.filter((chain: Chain) => chain.chainInfo.chainName?.toLowerCase() !== "injective")
-.map((chain: Chain) => {
-  // this is temporary given polygon RPC issues
-  const newChainInfo = chain.chainInfo;
-  if (newChainInfo?.chainName?.toLowerCase() === "polygon") {
-    newChainInfo.confirmLevel = 225
-    newChainInfo.estimatedWaitTime = 15
-  }
-  return newChainInfo
-})
+  .filter(
+    (chain: Chain) =>
+      (environment === "mainnet" ? chain.chainInfo.fullySupported : true) &&
+      !!chain?.chainInfo?.assets?.length &&
+      !disabledChains?.includes(chain.chainInfo.chainName.toLowerCase())
+  )
+  .filter(
+    (chain: Chain) => chain.chainInfo.chainName?.toLowerCase() !== "injective"
+  )
+  .map((chain: Chain) => {
+    // this is temporary given polygon RPC issues
+    const newChainInfo = chain.chainInfo
+    if (newChainInfo?.chainName?.toLowerCase() === "polygon") {
+      newChainInfo.confirmLevel = 225
+      newChainInfo.estimatedWaitTime = 15
+    }
+    return newChainInfo
+  })
 
-let bannedAddresses: string[] = []; 
+let bannedAddresses: string[] = []
 
-const { ethersJsConfigs } = getConfigs(environment) as ConfigsForEnvironment;
+const { ethersJsConfigs } = getConfigs(environment) as ConfigsForEnvironment
 
 for (const v of Object.values(ethersJsConfigs)) {
-  const { tokenAddressMap } = v as EthersJsConfigs;
+  const { tokenAddressMap } = v as EthersJsConfigs
   bannedAddresses = bannedAddresses.concat(Object.values(tokenAddressMap))
 }
 
@@ -41,5 +48,5 @@ export const ChainList = atom<ChainInfo[]>({
 
 export const BannedAddresses = atom<string[]>({
   key: "BannedAddresses",
-  default: bannedAddresses
+  default: bannedAddresses,
 })
